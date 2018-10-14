038b3b3852f25acc5ba5cc9e26ecd22ffc1c8c77
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2018-10-14 12:35:00 -0300

    Adding the loop

--- functions.php
+++ functions.php
@@ -6,3 +6,6 @@ function register_my_menu() {
     register_nav_menu('primary',__( 'Header Menu' ));
   }
   add_action( 'init', 'register_my_menu' );
+
+add_theme_support( 'post-thumbnails' ); 
+add_image_size( 'small-square', 288, 288, true );
--- index.php
+++ index.php
@@ -2,14 +2,33 @@
 
     <div class="container">
         <div class="blog-hero">
-            <h1 class="title-large">Aqui Compartilho meu dia-a-dia <br> e pensamentos sobre <a href="">design</a>, <br> <a href="">produtos digitais</a> e a <a href="">vida em geral</a></h1>
+            <p class="title-large"><?php echo get_bloginfo('description') ?></p>
         </div>
     </div>
     <div class="container">
         <div class="s-grid--home">
-            <div class="post-list">
-post list
+            <div class="home-post-list">
+                
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    <div class="home-post-item">
+                    
+                        <?php the_post_thumbnail( 'small-square', array('class' => 'home-post-thumbnail') ); ?>
+
+                        <div>
+                            <h2><a class="title-h2" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                            <p class="postmetadata"><?php the_time('d M,'); ?> por <?php the_author_posts_link(); ?> <?php _e( 'postado em' ); ?> <?php the_category( ', ' ); ?></p>
+                            <p><?php the_excerpt('30'); ?></p>
+                        </div>
+                    
+                    </div>
+                    <?php endwhile; ?>
+                        <div class="nav-previous alignleft"><?php previous_posts_link( 'Older posts' ); ?></div>
+                        <div class="nav-next alignright"><?php next_posts_link( 'Newer posts' ); ?></div>
+                    <?php else : ?>
+                        <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+                    <?php endif; ?>
             </div>
+
             <div class="sidebar">
 sidebar
             </div>
